364b44609e7bfb0d7ef4122d2b29628bbf776202
sammyatparachute <48567572+sammyatparachute@users.noreply.github.com>
2022-04-06 19:02:21 -0400

    first commit

--- public/app.js
+++ public/app.js
@@ -1,13 +1,27 @@
-// create map
+// Create map:                                                       
+const myMap = L.map('map').setView([48.868672, 2.342130],12);
 
 
-// add openstreetmap tiles
+// Add OpenStreetMap tiles:
+L.tileLayer('https://.tile.openstreetmap.org///.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    minZoom: '15',
+}).addTo(myMap)
 
 
-// create and main add geolocation marker
+
+// Create and add a geolocation marker:
+const marker = L.marker([48.87007, 2.346453])
+marker.addTo(myMap).bindPopup('<p1><b>The Hoxton, Paris</b></p1>').openPopup()
 
 
-// draw the 2nd arrondissement
+// Draw the 2nd Arrondissement                                          
+[48.863368120198004, 2.3509079846928516]
+[48.86933262048345, 2.3542531602919805]
+[48.87199261164275, 2.3400569901592183]
+[48.86993336274516, 2.3280142476578813] 
+[48.86834104280146, 2.330308418109664]
+
 
 
 // create red pin marker
